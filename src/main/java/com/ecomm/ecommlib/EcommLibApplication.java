308bf5962f4a3dae51b275ed72de143fb145bba7
keshancp <keshanc.perera@gmail.com>
2022-10-04 21:07:17 +0530

    initial commit for ecomm-lib

---
+++ src/main/java/com/ecomm/ecommlib/EcommLibApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.ecommlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomm/ecommlib/EcommLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.ecommlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
